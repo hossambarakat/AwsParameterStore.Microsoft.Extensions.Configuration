3d68022bc8012fc273dec05e82b29fd7d0a0c739
Hossam Barakat <hossam.eldeen@gmail.com>
2018-04-19 23:55:34 +1000

    Add more overloads to the extensions

--- Configuration.AwsParameterStore/AwsParameterStoreConfigurationExtensions.cs
+++ Configuration.AwsParameterStore/AwsParameterStoreConfigurationExtensions.cs
@@ -8,6 +8,21 @@ namespace Configuration.AwsParameterStore
     {
         public static IConfigurationBuilder AddAwsParameterStore(
             this IConfigurationBuilder configurationBuilder,
+            string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            return AddAwsParameterStore(
+                configurationBuilder,
+                path,
+                new DefaultAwsParameterManager(),
+                new AmazonSimpleSystemsManagementClient());
+        }
+        public static IConfigurationBuilder AddAwsParameterStore(
+            this IConfigurationBuilder configurationBuilder,
             string path,
             IAwsParameterManager parameterManager)
         {
@@ -15,12 +30,39 @@ namespace Configuration.AwsParameterStore
             {
                 throw new ArgumentNullException(nameof(path));
             }
+            if (parameterManager == null)
+            {
+                throw new ArgumentNullException(nameof(parameterManager));
+            }
+            return AddAwsParameterStore(
+                configurationBuilder,
+                path,
+                parameterManager,
+                new AmazonSimpleSystemsManagementClient());
+        }
 
-            var client = new AmazonSimpleSystemsManagementClient();
+        public static IConfigurationBuilder AddAwsParameterStore(
+            this IConfigurationBuilder configurationBuilder,
+            string path,
+            IAwsParameterManager parameterManager, IAmazonSimpleSystemsManagement amazonSimpleSystemsManagementClient)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+            if (parameterManager == null)
+            {
+                throw new ArgumentNullException(nameof(parameterManager));
+            }
+            if (amazonSimpleSystemsManagementClient == null)
+            {
+                throw new ArgumentNullException(nameof(amazonSimpleSystemsManagementClient));
+            }
+
             configurationBuilder.Add(new AwsParameterStoreConfigurationSource
             {
                 Path = path,
-                Client = client,
+                Client = amazonSimpleSystemsManagementClient,
                 ParameterManager = parameterManager
             });
             return configurationBuilder;
